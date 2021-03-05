0c1fdbe69ba92e58f8cf5a28e8276ee26c2e60bc
alexdanieldm <31324674+alexdanieldm@users.noreply.github.com>
2021-03-05 17:28:30 -0400

    add navbar behaviour

---
+++ public/script/navbar.js
@@ -0,0 +1,14 @@
+
+$(document).ready(function() {
+
+    $(window).scroll(function() {
+        
+        if($(this).scrollTop() > 500) { 
+          $('#navbar').addClass('solid');
+          
+        } else {
+          $('#navbar').removeClass('solid');
+        }
+
+    });
+});
